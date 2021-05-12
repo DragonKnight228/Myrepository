00d2c8a57f8f28ae34b6e66de29b3cc2448a7fb9
Никита <redrenjer@yandex.ru>
2021-05-12 20:11:20 +0300

    Теперь точно работает

--- cod.cpp
+++ cod.cpp
@@ -5,8 +5,9 @@ using namespace std ;
 int main()
 {
      int a, b;
-     cin >> a, b;
-     cout << a + b;
-	 cout << a - b;
+     cin >> a;
+     cin >> b;
+     cout << a + b << "\n";
+     cout << a - b;
 }
 
